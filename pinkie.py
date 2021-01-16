f0a73383520808218a5378275b5a607df1baa0d9
Scripsi <scripsi@scripsi.uk>
2021-01-16 11:15:20 +0000

    created background image to paste into

--- pinkie.py
+++ pinkie.py
@@ -17,8 +17,18 @@ Displaying the world in the Kitchen
 
 inky = Inky()
 
+img = Image.open("/home/pi/pinkie/background.png")
+draw = ImageDraw.Draw(img)
+
 virus_img = Image.open("/home/pi/pinkie/virus.png")
 duck_img = Image.open("/home/pi/pinkie/duck.png")
 
-inky.set_image(duck_img)
+x = random.randint(0, inky.WIDTH-51)
+y = random.randint(0, inky.HEIGHT-51)
+img.paste(virus_img,(x,y))
+x = random.randint(0, inky.WIDTH-51)
+y = random.randint(0, inky.HEIGHT-51)
+img.paste(duck_img,(x,y))
+
+inky.set_image(img)
 inky.show()
