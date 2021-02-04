17276715187a081962bcdffd717427887fe93e42
Scripsi <scripsi@scripsi.uk>
2021-02-04 11:44:40 +0000

    Created screen test

--- pinkie.py
+++ pinkie.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
 
 # import lots of necessary stuff
-from PIL import Image, ImageFont, ImageDraw
-from font_fredoka_one import FredokaOne
-from datetime import datetime, timedelta
+import PIL
 import schedule
-import re
-import random
-import pandas as pd
 from inky.inky_uc8159 import Inky
 from gpiozero import Button
 from signal import pause
+import screen_test
 
 print("""PinKIE
 
@@ -24,26 +20,22 @@ button_b = Button(6)
 button_c = Button(16)
 button_d = Button(24)
 
-# img = Image.open("/home/pi/pinkie/background.png")
-draw = ImageDraw.Draw(img)
-
-virus_img = Image.open("/home/pi/pinkie/virus.png")
-duck_img = Image.open("/home/pi/pinkie/duck.png")
-
 def show_image(image_to_show):
-    x = random.randint(0, inky.WIDTH-51)
-    y = random.randint(0, inky.HEIGHT-51)
-    img.paste(image_to_show,(x,y))
-    inky.set_image(img)
-    inky.show()
 
-def show_virus():
-    show_image(virus_img)
+    if image_to_show == "test":
+        screen_test.update_image
+        inky.set_image(screen_test.get_image)
+        inky.show()
+    else:
+        inky.clear()
 
-def show_duck():
-    show_image(duck_img)
+def show_test:
+    show_image(test)
 
-button_a.when_released = show_virus
-button_b.when_released = show_duck
+def show_clear:
+    show_image(clear)
+
+button_a.when_released = show_test
+button_b.when_released = show_clear
 
 pause()
